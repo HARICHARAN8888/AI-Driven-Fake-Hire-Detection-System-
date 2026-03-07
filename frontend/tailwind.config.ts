c09285103b86157904e6ac9b852690c52cd1364d
Vasanth2005kk <vasanth2005kk@gmail.com>
2026-03-07 15:08:50 +0530

    tailwind css issue solved

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,33 +1,42 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-  	extend: {
-  		colors: {
-			background: '#070718',
-			surface: 'rgba(255, 255, 255, 0.05)',
-			primary: {
-				DEFAULT: '#6C63FF',
-				glow: 'rgba(108, 99, 255, 0.35)',
+	darkMode: ["class"],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				border: "hsl(var(--border))",
+				foreground: "hsl(var(--foreground))",
+				background: "#070718",
+
+				surface: "rgba(255,255,255,0.05)",
+
+				primary: {
+					DEFAULT: "#6C63FF",
+					glow: "rgba(108, 99, 255, 0.35)",
+				},
+
+				secondary: {
+					DEFAULT: "#00D1FF",
+				},
+
+				textPrimary: "#EAEAF0",
+				danger: "#FF4D6D",
+				success: "#00FFA3",
 			},
-			secondary: {
-				DEFAULT: '#00D1FF',
+
+			backgroundImage: {
+				"cyber-gradient":
+					"linear-gradient(to bottom right, #070718, #0B0B1A)",
 			},
-			textPrimary: '#EAEAF0',
-			danger: '#FF4D6D',
-			success: '#00FFA3',
-  		},
-		backgroundImage: {
-			'cyber-gradient': 'linear-gradient(to bottom right, #070718, #0B0B1A)',
-		}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
+		},
+	},
+	plugins: [require("tailwindcss-animate")],
 };
+
 export default config;
